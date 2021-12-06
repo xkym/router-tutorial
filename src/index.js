f542f8cafee2626cbb37e14589236816bd73b3f6
xkym <xky1000@qq.com>
2021-12-07 06:59:04 +0800

    add some routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import { render } from "react-dom";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
 
 const rootElement = document.getElementById("root");
-render(<BrowserRouter><App /></BrowserRouter>, rootElement);
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />} />
+    </Routes>
+  </BrowserRouter>, rootElement);
