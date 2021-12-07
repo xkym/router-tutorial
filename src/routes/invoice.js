8ad2ad55dbb7de8c206d825fa635ce6a0ceba807
xkym <xky1000@qq.com>
2021-12-07 11:26:50 +0800

    navigate programmatically

--- src/routes/invoice.js
+++ src/routes/invoice.js
@@ -1,6 +1,7 @@
-import { useParams } from "react-router";
-import { getInvoice } from "../data";
+import { useNavigate, useParams } from "react-router";
+import { getInvoice, deleteInvoice } from "../data";
 export default function Invoice() {
+  let navigate = useNavigate()
   let params = useParams()
   let invoice = getInvoice(parseInt(params.invoiceId, 10));
   return (
@@ -10,6 +11,14 @@ export default function Invoice() {
         {invoice.name}: {invoice.number}
       </p>
       <p>Due Date: {invoice.due}</p>
+      <button
+        onClick={() => {
+          deleteInvoice(invoice.number);
+          navigate("/invoices");
+        }}
+      >
+        Delete
+      </button>
     </main>
   );
 }
