9b13a8bd156a5a1f613d944f6fc6460501806aea
xkym <xky1000@qq.com>
2021-12-07 11:14:43 +0800

    add active link

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,26 +1,32 @@
-import { Link ,Outlet} from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import { getInvoices } from "../data";
 export default function Invoices() {
-    let invoices=getInvoices()
-    return (
-        <div style={{ display: "flex" }}>
-        <nav
-          style={{
-            borderRight: "solid 1px",
-            padding: "1rem"
-          }}
-        >
-          {invoices.map(invoice => (
-            <Link
-              style={{ display: "block", margin: "1rem 0" }}
-              to={`/invoices/${invoice.number}`}
-              key={invoice.number}
-            >
-              {invoice.name}
-            </Link>
-          ))}
-        </nav>
-        <Outlet></Outlet>
-      </div>
-    );
-  }
+  let invoices = getInvoices()
+  return (
+    <div style={{ display: "flex" }}>
+      <nav
+        style={{
+          borderRight: "solid 1px",
+          padding: "1rem"
+        }}
+      >
+        {invoices.map(invoice => (
+          <NavLink
+            style={({ isActive }) => {
+              return {
+                display: "block",
+                margin: "1rem 0",
+                color: isActive ? "red" : ""
+              };
+            }}
+            to={`/invoices/${invoice.number}`}
+            key={invoice.number}
+          >
+            {invoice.name}
+          </NavLink>
+        ))}
+      </nav>
+      <Outlet></Outlet>
+    </div>
+  );
+}
